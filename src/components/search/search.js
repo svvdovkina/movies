4e937979371c54380833a80d5db6df95934eb8ec
Svetlana Vdovkina <svetlanavdovkina@Svetlanas-MacBook-Air.local>
2023-04-09 19:26:39 +0300

    add search component

---
+++ src/components/search/search.js
@@ -0,0 +1,42 @@
+import { DivComponent } from "../../common/div-component";
+import "./search.css"
+
+export class Search extends DivComponent {
+    constructor(state) {
+        super();
+        this.state = state;
+    }
+
+    search() {
+        const val = this.el.querySelector("input").value;
+        this.state.searchQuery = val;
+        console.log(val);
+    }
+
+    render() {
+        this.el.classList.add("search-bar");
+        this.el.innerHTML = `
+        <div class="search-wrapper">
+            <img src="static/search.svg" alt="search-pic" />
+            <input 
+                type="text" 
+                placeholder="Search movie..."
+                class="search-input"
+                value="${this.state.searchQuery ? this.state.searchQuery :''}"
+            />
+        </div>
+        <button class="search-but">
+            <img src="static/search-white.svg" alt="search-pic" />
+        </button>`;
+        this.el.querySelector('.search-but').addEventListener('click', ()=>this.search());
+        this.el.querySelector('input').addEventListener('keydown', (e)=>{
+            if (e.key === "Enter") {
+                e.preventDefault();
+                this.search();
+            }
+        });
+
+   
+        return this.el
+    }
+}
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,6 +1,7 @@
 import { AbstractView } from "../../common/view.js"
 import onChange from 'on-change'
 import { Header } from "../../components/header/header.js";
+import { Search } from "../../components/search/search.js";
 
 export class MainView extends AbstractView {
     
@@ -8,26 +9,44 @@ export class MainView extends AbstractView {
         list: [],
         loading: false,
         searchQuery: undefined,
-        offset: 0
+        page: 0
     };
     
     constructor(appState) {
         super();
         this.appState = appState;
         this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.state = onChange(this.state, this.stateHook.bind(this));
         this.setTitle("Movies search");
+    }
+
+    async loadList(title="Batman", page=1) {
+        const req = await fetch(`https://www.omdbapi.com/?apikey=21647379&s=${title}&page=${page}`);
+        return req.json();
+        
+    }
+
+    async stateHook(path){
+        if (path == 'searchQuery') {
+            this.state.loading = true;
+            const data = await this.loadList(this.state.searchQuery);
+            console.log(data);
+            this.state.loading = false;
+            this.state.list = data;
+        }
+        
     }
 
     appStateHook(path) {
         console.log(path);
-        if (path =='favorites' ) {
+        if (path == 'favorites' ) {
             this.render()
         }
     }
 
     render() {
         const main = document.createElement('div');
-        main.innerText = `Favorite movies: ${this.appState.favorites.length}`;
+        main.append(new Search(this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
